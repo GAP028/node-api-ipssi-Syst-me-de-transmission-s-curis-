9f58a4d25aed823d4b0908df74583d55713eff03
Gomas <gomasalainpatrick8@gmail.com>
2026-03-19 19:21:41 +0100

    modif index et ajout authmiddleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connection = require('./db');
 require('dotenv').config();
 const messageRouter = require('./routes/messageRoutes');
+const authRouter = require('./routes/authRoute');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'API transmission active'});
 });
 app.use('/message', messageRouter);
+app.use('/auth', authRouter);
 app.listen(process.env.PORT || 3000, () => {
     console.log ("ca fonctionne");
 });
